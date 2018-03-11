d36bbac32667491e318522e99288f17e48a8d267
Vinicius Noda <noda.94@gmail.com>
2018-03-11 15:05:49 -0300

    validations and insertions in create user

--- app/controller/controlPanelController.php
+++ app/controller/controlPanelController.php
@@ -1,10 +1,24 @@
 <?php
 require_once "../config.php";
-require_once "../model/post.class.php";
+require_once "../model/user.class.php";
 
-switch($_post["tag"]){
+print_r($_POST);
+
+switch($_POST["tag"]){
     case "createUser":
+        $user = new User;
+        $user->setName($_POST["name"]);
+        $user->setNickname($_POST["username"]);
+        $user->setPassword($_POST["password"]);
+        $user->setPrivilege($_POST["isAdmin"]);
         
+        if($user->checkDuplicatedNick($db)){
+            echo "duplicated";
+            return;
+        }
+
+        echo($user->createNewUser($db));
+
         break;
 }
 
